dc528b5ab6ad8aa9485d74dfb0910d1cbda62dea
Hongdi-LAI <69794567+Hongdi-LAI@users.noreply.github.com>
2020-09-13 17:58:21 +0200

    Payment API Setup with Express and Cloud Function

---
+++ functions/index.js
@@ -0,0 +1,15 @@
+//Build express app and host on cloud function
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')('sk_test_51HQvwyD2WNwr7ngMrdurQgNIoUjmhuq4fiqg1MC9El3b3F8MbpMdi4XLHijV1LhznJZmy6MjVN1jkuSVhgAE4wJe00DaRZD8Xg');
+
+//API
+
+//APP CONFIG
+
+//Middlewares
+
+//API Routes
+
+//Listen Command
