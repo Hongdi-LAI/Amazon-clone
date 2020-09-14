d0774b749e70c10e4d126b958d074d6dbf759fe4
Hongdi-LAI <69794567+Hongdi-LAI@users.noreply.github.com>
2020-09-14 13:02:11 +0200

    Firebase Backend Deploy and Swap New  Deployed API

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 
 const instance = axios.create({
     //CLOUD FUNCTION API URL
-    baseURL: 'http://localhost:5001/clone-27d1c/us-central1/api'
+    //baseURL: 'http://localhost:5001/clone-27d1c/us-central1/api'
+    baseURL: 'https://us-central1-clone-27d1c.cloudfunctions.net/api'
 });
 
 export default instance;
