ab5aa57c07e30439268c0187ce948943ef55333d
Hongdi Lai <69794567+Hongdi-LAI@users.noreply.github.com>
2020-09-15 01:50:52 +0200

    Update axios.js
    Firebase Cloud Function API hidden for security reasons

--- src/axios.js
+++ src/axios.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const instance = axios.create({
     //CLOUD FUNCTION API URL
     //baseURL: 'http://localhost:5001/clone-27d1c/us-central1/api'
-    baseURL: 'https://us-central1-clone-27d1c.cloudfunctions.net/api'
+    baseURL: 'your api'
 });
 
 export default instance;
